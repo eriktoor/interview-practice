eecb75d7a3c31d0f4a6dc84469146190e065f015
erik.toor <eriktoor@gmail.com>
2020-02-03 12:45:08 -0500

    renames

--- dp/coinChange.py
+++ dp/coinChange.py
@@ -58,3 +58,63 @@ def countOfWaysToMakeChange(arr_coins, num_coins, n): #top down m
 arr = [1, 2, 3] 
 m = len(arr) #m is the number of coins 
 print(countOfWaysToMakeChange(arr, m, 4)) #n is the value we are trying to create
+
+
+
+
+
+
+
+
+
+
+def minChange(coins, value):
+    res = []    
+    def findMinChange(coins, value, num):
+        if value == 0: 
+            res.append(num)
+        
+        for i in coins: 
+            if i <= value: 
+                findMinChange(coins, value - i, num + 1)
+        
+        return res
+    
+    findMinChange(coins, value, 0)
+    print(res)
+    return min(res)
+    
+
+
+
+arr = [1, 2, 3] 
+print(minChange(arr, 5)) #n is the value we are trying to create
+
+
+
+def minChange(coins, value):
+    res = []
+    memo = [value + 1 for i in range(value + 1)]
+    def findMinChange(coins, value, num):
+        if value in memo: 
+            return memo[value]
+        
+        if value == 0: 
+            res.append(num)
+        
+        for i in coins: 
+            if i <= value: 
+                dp = findMinChange(coins, value - i, num + 1)
+                memo[i] = dp
+        
+        return res
+    
+    findMinChange(coins, value, 0)
+    print(res)
+    return min(res)
+    
+
+
+
+arr = [1, 2, 3] 
+print(minChange(arr, 5)) #n is the value we are trying to create
