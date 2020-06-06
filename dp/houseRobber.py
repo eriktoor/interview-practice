13407c2146f29e6726bb435ca581aaca1c3365a8
Erik Toor <eriktoor@gmail.com>
2020-06-06 13:35:10 -0400

    Update houseRobber.py

--- dp/houseRobber.py
+++ dp/houseRobber.py
@@ -1,17 +1,88 @@
-class SolutionRobber:
-    def robTopDownDP(self, hval): #TOP DOWN WITH MEMO 
+class Solution:
+    
+    """
+    
+    Approaches:
+    
+    Approach 1: Top Down 
+    
+    State: 
+        - hval 
+        - currentHouse * (value you are changing)
+    
+    Base Cases: 
+        - if there are no houses: can't pick anything 
+        - if there is 1 houses: pick it 
+        - if there is 2 houess: pick the max value house
+        
+    Decisions: 
+        - pick the current house and the house two houses before 
+        - pick the house 1 before 
+    
+    Pseudocode: 
+    def rob(self, hval):
+        def helper(hval, i):
+            if i < 0: return 0 
+            
+            # pick_house = hval[i] + helper(hval, i - 2)
+            # pick_house_before = helper(hval, i - 1)
+            # return max(pick_house, pick_house_before)
+            return max(hval[i] + helper(hval,i-2), helper(hval, i-1))
+        
+    return helper(hval, len(hval)-1)
+    
+
+    Time: O(2^n), Space: O(2^n) calls on the call stack 
+    
+    Decision Tree
+    [1,2,3,1]
+                    h(hval, 4)
+                   /         \ 
+         h(hval, 3)         h(hval, 2) + hval[4]
+        /       \                 /             \
+h(hval, 2)  h(hval,1) +hval[3]  hval(hval, 1)   h(hval, 0) + hval[2]
+
+
+
+    Approach 1: Top Down with Memoization 
+    
+    Same exact approach, but storing the results of completed subproblems 
+    
+    
+    Pseudocode: 
+    def rob(self, hval):
+        memo = {}
+        def helper(hval, i):
+            if i in memo: return memo[i]
+            if i < 0: return 0 
+            
+            # pick_house = hval[i] + helper(hval, i - 2)
+            # pick_house_before = helper(hval, i - 1)
+            # return max(pick_house, pick_house_before)
+            memo[i] =  max(hval[i] + helper(hval,i-2), helper(hval, i-1))
+            return memo[i]
+    return helper(hval, len(hval)-1)
+    
+
+
+    """ 
+    
+    
+    def robTopDownDP(self, hval): 
         def helper(hval, i):
             if i < 0: return 0
             elif memo[i] >= 0: return memo[i]
             
-            res = max(helper(hval, i-2) + hval[i], helper(hval, i -1))
-            memo[i] = res 
-            return res 
-            
+            #res = max(helper(hval, i-2) + hval[i], helper(hval, i -1))
+            #memo[i] = res 
+            #return res 
+            memo[i] = max(helper(hval, i-2) + hval[i], helper(hval, i -1))
+            return memo[i]
+        
         memo = [-1 for i in range(len(hval) + 1)]
         return helper(hval, len(hval) - 1)
 
-    def robTopDown(self, hval): #NO MEMO 
+    def robTopDown(self, hval): 
         def helper(hval, i):
             if i < 0: return 0
 
@@ -21,7 +92,7 @@ class SolutionRobber:
             
         return helper(hval, len(hval) - 1)
     
-    def rob(self, hval): #DP BOTTOM UP 
+    def robBottomUp(self, hval): 
         if len(hval) == 0: return 0
         elif len(hval) == 1: return hval[0]
         elif len(hval) == 2: return max(hval[0], hval[1]) 
